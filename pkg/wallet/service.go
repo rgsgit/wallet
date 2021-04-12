8bad87d0d63d51b4b6a51ce93418ac8f6bda1b78
Ravshan Sharifov <sharifovrav@gmail.com>
2021-04-12 15:18:04 +0500

    RegisterAccount

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -2,7 +2,24 @@ package wallet
 
 import "github.com/rgsgit/wallet/pkg/types"
 
-type Serivice struct{
-	accounts []types.Account
-	payments []types.Payment
+
+type Service struct{
+	nextAccountID	int64
+	accounts 		[]types.Account
+	payments 		[]types.Payment
+}
+
+func RegisterAccount(service *Service, phone types.Phone)  {
+	for _, account :=service.accounts {
+		if account.phone == phone {
+			return
+		}
+	}
+	
+	service.nextAccountID++
+	service.accounts = append(service.accounts, types.Account{
+		ID:			service.nextAccountID,
+		Phone: 		phone,
+		Balance: 	0,
+	})
 }
